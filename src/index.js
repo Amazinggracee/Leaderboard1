b6de4dd47543e1b844de5f76ce80831c0279a9c8
Amarachi Dimkpa <amaradimkpa1@gmail.com>
2023-04-10 22:34:12 +0100

    Add my modules and the src files

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import './style.css';
+import Store from './modules/store.js';
+import UI from './modules/design.js';
+import User from './modules/user.js';
+
+// display users onload of the page
+document.addEventListener('DOMContentLoaded', UI.displayUsers);
+
+const form = document.querySelector('#user-form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const name = document.querySelector('#name').value;
+  const score = document.querySelector('#score').value;
+
+  const user = new User(name, score);
+
+  Store.addUser(user);
+  UI.addUserToList(user);
+
+  UI.clearFields();
+});
